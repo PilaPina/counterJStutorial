7af02d4f9bf48dc4d9a53cfaf783aaa26344032c
PilaPina <solald@gmail.com>
2023-09-20 14:17:08 +0000

    small change

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 // 1. Create two variables, myAge and humanDogRatio
 // 2. Multiply the two together and store the result in myDogAge 
 // 3. Log myDogAge to the console
-// let myAge = 47
+// let myAge = 37
 // let humanDogRatio = 7
 // let myDogAge = myAge * humanDogRatio
 // console.log(myDogAge)
